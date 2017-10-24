9885aa3677d89fa7d3ef282ce34196afc4d0e0f6
Julien Chichignoud <jchichignoud@gmail.com>
2017-10-24 12:49:34 +1100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global $, window, location, CSInterface, SystemPath, themeManager*/
+
+(function () {
+    'use strict';
+
+    var csInterface = new CSInterface();
+    
+    
+    function init() {
+                
+        themeManager.init();
+                
+        $("#btn_duplicate").click(function () {
+            csInterface.evalScript('duplicateActive()');
+        });
+    }
+        
+    init();
+
+}());
+    
---
+++ jsx/hostscript.jsx
@@ -0,0 +1,78 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global $, Folder*/
+
+
+//function sayHello(){
+//    alert("hello from ExtendScript");
+//}
+
+var sequenceLocation = app.project.rootItem.children[0]
+var sequenceName = null
+
+duplicateActive();
+
+
+
+function getSequenceID(){
+    var seqID = app.project.activeSequence.projectItem.nodeId;
+    return seqID;
+}
+
+function duplicateActive(){
+	sequenceName = app.project.activeSequence.projectItem.name;
+    findInProjectById(getSequenceID());
+    app.project.activeSequence.clone();
+    app.project.activeSequence.projectItem.name = incrementName(sequenceName);
+    app.project.activeSequence.projectItem.moveBin(sequenceLocation);
+}
+
+
+
+function findInProjectById(id){
+    sequenceLocation = app.project.rootItem;
+    var checkEachProjectItem = function(element){
+        for (var i = 0; i < element.children.numItems; i++){
+            if (element.children[i].type === 2){
+                checkEachProjectItem(element.children[i]);
+            } else {
+                if (element.children[i].nodeId === id){
+                	sequenceLocation = element
+                	element.children[i].moveBin(findOrCreateArchiveBin(sequenceLocation))
+                	return
+                }
+            }
+        }
+    }
+    return checkEachProjectItem(app.project.rootItem);
+}
+
+
+//returns location of the bin
+function findOrCreateArchiveBin(sequenceBin){
+	for (var i = 0; i < sequenceBin.children.numItems; i++){
+		if (sequenceBin.children[i].name === "_OLD"){
+			return sequenceBin.children[i];
+		}
+	}
+	return sequenceBin.createBin("_OLD");
+}
+
+function incrementName(string){
+	const numberSplitRegex = /^(.*?)(\d+)$/g;
+	var digitNumber = null;
+	var splitNameArray = numberSplitRegex.exec(string);
+	newNumber = parseInt(splitNameArray[2], 10) + 1;
+	if (newNumber.toString().length > splitNameArray[2].length){
+		digitNumber = newNumber.toString().length;
+	} else {
+		digitNumber = splitNameArray[2].length;
+	}
+	newNumber = padNum(newNumber, digitNumber)
+	return splitNameArray[1] + newNumber;
+}
+
+
+function padNum(num, size) {
+    var s = "000000000" + num;
+    return s.substr(s.length-size);
+}
