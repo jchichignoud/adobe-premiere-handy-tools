628f2cbeb03dce6ba847876e0b57e3a84ac75ee7
Julien Chichignoud <jchichignoud@gmail.com>
2018-08-18 15:58:59 +1000

    error handling

--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -1,11 +1,11 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, Folder*/
 
-if (ExternalObject.AdobeXMPScript === undefined) {  
-     ExternalObject.AdobeXMPScript = new ExternalObject('lib:AdobeXMPScript');  
-}  
+if (ExternalObject.AdobeXMPScript === undefined) {
+	ExternalObject.AdobeXMPScript = new ExternalObject('lib:AdobeXMPScript');
+}
 
-var kPProPrivateProjectMetadataURI	= "http://ns.adobe.com/premierePrivateProjectMetaData/1.0/"
+var kPProPrivateProjectMetadataURI = "http://ns.adobe.com/premierePrivateProjectMetaData/1.0/"
 
 ///////////////////////////////////
 //////DUPLICATE AND INCREMENT//////
@@ -15,68 +15,126 @@ var kPProPrivateProjectMetadataURI	= "http://ns.adobe.com/premierePrivateProject
 var sequenceLocation = app.project.rootItem.children[0];
 var sequenceName = null;
 
-function getSequenceID(){
-    var seqID = app.project.activeSequence.projectItem.nodeId;
-    return seqID;
+function getSequenceID() {
+	return app.project.activeSequence.projectItem.nodeId;
 }
 
-function duplicateActive(){
-	sequenceName = app.project.activeSequence.projectItem.name;
-    findInProjectById(getSequenceID());
-    app.project.activeSequence.clone();
-    app.project.activeSequence.projectItem.name = incrementName(sequenceName);
-    app.project.activeSequence.projectItem.moveBin(sequenceLocation);
+function duplicateActive() {
+	if (app.project.activeSequence) {
+		sequenceName = app.project.activeSequence.projectItem.name;
+		findInProjectById(getSequenceID());
+		app.project.activeSequence.clone();
+		app.project.activeSequence.projectItem.name = incrementName(sequenceName);
+		app.project.activeSequence.projectItem.moveBin(sequenceLocation);
+	} else {
+		alert ("No Active Sequence")
+	}
 }
 
 
-
-function findInProjectById(id){
-    sequenceLocation = app.project.rootItem;
-    var checkEachProjectItem = function(element){
-        for (var i = 0; i < element.children.numItems; i++){
-            if (element.children[i].type === 2){
-                checkEachProjectItem(element.children[i]);
-            } else {
-                if (element.children[i].nodeId === id){
-                	sequenceLocation = element
-                	element.children[i].moveBin(findOrCreateArchiveBin(sequenceLocation))
-                	return
-                }
-            }
-        }
-    }
-    return checkEachProjectItem(app.project.rootItem);
+function findInProjectById(id) {
+	sequenceLocation = app.project.rootItem;
+	var checkEachProjectItem = function (element) {
+		for (var i = 0; i < element.children.numItems; i++) {
+			if (element.children[i].type === 2) {
+				checkEachProjectItem(element.children[i]);
+			} else {
+				if (element.children[i].nodeId === id) {
+					sequenceLocation = element
+					element.children[i].moveBin(findOrCreateArchiveBin(sequenceLocation))
+					return
+				}
+			}
+		}
+	}
+	return checkEachProjectItem(app.project.rootItem);
 }
 
 
 //returns location of the bin
-function findOrCreateArchiveBin(sequenceBin){
-	for (var i = 0; i < sequenceBin.children.numItems; i++){
-		if (sequenceBin.children[i].name === "_OLD"){
+function findOrCreateArchiveBin(sequenceBin) {
+	for (var i = 0; i < sequenceBin.children.numItems; i++) {
+		if (sequenceBin.children[i].name === "_OLD") {
 			return sequenceBin.children[i];
 		}
 	}
 	return sequenceBin.createBin("_OLD");
 }
 
-function incrementName(string){
+function incrementName(string) {
 	const numberSplitRegex = /^(.*?)(\d+)$/g;
 	var digitNumber = null;
 	var splitNameArray = numberSplitRegex.exec(string);
-	newNumber = parseInt(splitNameArray[2], 10) + 1;
-	if (newNumber.toString().length > splitNameArray[2].length){
-		digitNumber = newNumber.toString().length;
+
+	if (!splitNameArray) {
+		// if there was no digit at the end of name, add _v2 to the new sequence name
+		return string + "_v2"
 	} else {
-		digitNumber = splitNameArray[2].length;
+		newNumber = parseInt(splitNameArray[2], 10) + 1;
+		if (newNumber.toString().length > splitNameArray[2].length) {
+			digitNumber = newNumber.toString().length;
+		} else {
+			digitNumber = splitNameArray[2].length;
+		}
+		newNumber = padNum(newNumber, digitNumber)
+		return splitNameArray[1] + newNumber;
 	}
-	newNumber = padNum(newNumber, digitNumber)
-	return splitNameArray[1] + newNumber;
 }
 
 
 function padNum(num, size) {
-    var s = "000000000" + num;
-    return s.substr(s.length-size);
+	var s = "000000000" + num;
+	return s.substr(s.length - size);
+}
+
+///////////////////////////////////
+////////////SNAPSHOT///////////////
+///////////////////////////////////
+
+
+function snapshotSequence(date, description) {
+	sequenceName = app.project.activeSequence.projectItem.name;
+	findInProjectById_v2(getSequenceID());
+	app.project.activeSequence.clone();
+	app.project.activeSequence.projectItem.name = date + "_" + sequenceName;
+	addNotes(app.project.activeSequence, description);
+	app.project.activeSequence.projectItem.moveBin(findOrCreateSnapshotsBin(sequenceLocation));
+}
+
+
+function findOrCreateSnapshotsBin(sequenceBin) {
+	for (var i = 0; i < sequenceBin.children.numItems; i++) {
+		if (sequenceBin.children[i].name === "_SNAPSHOTS") {
+			return sequenceBin.children[i];
+		}
+	}
+	return sequenceBin.createBin("_SNAPSHOTS");
+}
+
+function findInProjectById_v2(id) {
+	sequenceLocation = app.project.rootItem;
+	var checkEachProjectItem = function (element) {
+		for (var i = 0; i < element.children.numItems; i++) {
+			if (element.children[i].type === 2) {
+				checkEachProjectItem(element.children[i]);
+			} else {
+				if (element.children[i].nodeId === id) {
+					sequenceLocation = element;
+					element.children[i].moveBin(sequenceLocation)
+					return
+				}
+			}
+		}
+	}
+	return checkEachProjectItem(app.project.rootItem);
+}
+
+
+function addNotes(activeSeq, note) {
+	var xmp = new XMPMeta(activeSeq.projectItem.getProjectMetadata());
+	xmp.setProperty(kPProPrivateProjectMetadataURI, 'Column.PropertyText.Description', note);
+	var xmpAsString = xmp.serialize()
+	activeSeq.projectItem.setProjectMetadata(xmpAsString, ['Column.PropertyText.Description']);
 }
 
 ///////////////////////////////////
@@ -86,11 +144,11 @@ function padNum(num, size) {
 function fillFrame() {
 	var seq = app.project.activeSequence;
 	var seqSize = getSize(seq);
-	for (var tr = 0; tr < seq.videoTracks.numTracks; tr++){
+	for (var tr = 0; tr < seq.videoTracks.numTracks; tr++) {
 		var trackClips = seq.videoTracks[tr].clips;
-		for (var cl = 0; cl < trackClips.numItems; cl++){
+		for (var cl = 0; cl < trackClips.numItems; cl++) {
 			var clip = trackClips[cl]
-			if (clip.isSelected()){
+			if (clip.isSelected()) {
 				var clipSize = getSize(clip);
 				var scale = clip.components[1].properties[1]
 				if (clipSize.ratio > seqSize.ratio) {
@@ -109,59 +167,12 @@ function fillFrame() {
 // takes in the active sequence or a sequence clip, returns object with width, height and ratio properties of the item
 function getSize(item) {
 	var xmp = new XMPMeta(item.projectItem.getProjectMetadata())
-					var sizeInfo = xmp.getProperty(kPProPrivateProjectMetadataURI, 'Column.Intrinsic.VideoInfo').toString().split(' ');
-					var width = sizeInfo[0];
-					var height = sizeInfo[2]
-					return({'width': width, 'height': height, 'ratio': (width/height)})
-}
-
-
-///////////////////////////////////
-////////////SNAPSHOT///////////////
-///////////////////////////////////
-
-
-function snapshotSequence(date, description){
-	sequenceName = app.project.activeSequence.projectItem.name;
-	findInProjectById_v2(getSequenceID());
-    app.project.activeSequence.clone();
-	app.project.activeSequence.projectItem.name = date + "_" + sequenceName;
-	addNotes(app.project.activeSequence, description);
-    app.project.activeSequence.projectItem.moveBin(findOrCreateSnapshotsBin(sequenceLocation));
-}
-
-
-function findOrCreateSnapshotsBin(sequenceBin){
-	for (var i = 0; i < sequenceBin.children.numItems; i++){
-		if (sequenceBin.children[i].name === "_SNAPSHOTS"){
-			return sequenceBin.children[i];
-		}
-	}
-	return sequenceBin.createBin("_SNAPSHOTS");
-}
-
-function findInProjectById_v2(id){
-    sequenceLocation = app.project.rootItem;
-    var checkEachProjectItem = function(element){
-        for (var i = 0; i < element.children.numItems; i++){
-            if (element.children[i].type === 2){
-                checkEachProjectItem(element.children[i]);
-            } else {
-                if (element.children[i].nodeId === id){
-                	sequenceLocation = element;
-                	element.children[i].moveBin(sequenceLocation)
-                	return
-                }
-            }
-        }
-    }
-    return checkEachProjectItem(app.project.rootItem);
-}
-
-
-function addNotes(activeSeq, note) {
-	var xmp = new XMPMeta(activeSeq.projectItem.getProjectMetadata());
-	xmp.setProperty(kPProPrivateProjectMetadataURI, 'Column.PropertyText.Description', note);
-	var xmpAsString = xmp.serialize()
-	activeSeq.projectItem.setProjectMetadata(xmpAsString, ['Column.PropertyText.Description']);
+	var sizeInfo = xmp.getProperty(kPProPrivateProjectMetadataURI, 'Column.Intrinsic.VideoInfo').toString().split(' ');
+	var width = sizeInfo[0];
+	var height = sizeInfo[2]
+	return ({
+		'width': width,
+		'height': height,
+		'ratio': (width / height)
+	})
 }
